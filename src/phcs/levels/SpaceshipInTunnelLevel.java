2cef90196d0f9223b5f0b87850c04e768cdcac6f
matrixfrog <matrixfrog@41e188f2-1ff7-11df-8982-67e453f37615>
2010-03-27 03:46:17 +0000

    refactoring and a little testing

---
+++ src/phcs/levels/SpaceshipInTunnelLevel.java
@@ -0,0 +1,24 @@
+package phcs.levels;
+
+import static phcs.PhysicalObject.inverseGamma;
+import phcs.gui.TunnelController;
+import phcs.objects.Spaceship;
+import phcs.objects.Tunnel;
+import util.swingutils.RecursiveEnablePanel;
+
+public class SpaceshipInTunnelLevel extends LightClocksOnTrainLevel {
+  public SpaceshipInTunnelLevel() {
+    setName("Level 2: Spaceship in a Tunnel");
+
+    Tunnel tunnel = new Tunnel(500, 285, 50, 30, 0, 0);
+    this.addSimulationObject(tunnel);
+
+    Spaceship spaceship = new Spaceship(30, 290, 100, 20, inverseGamma(2), 0);
+    this.addSimulationObject(spaceship);
+
+    this.controlPanel = new RecursiveEnablePanel();
+    TunnelController controller = new TunnelController();
+    tunnel.setController(controller);
+    this.controlPanel.add(controller);
+  }
+}
