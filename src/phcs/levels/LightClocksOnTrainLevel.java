f0f0a312c7eeb5db56211987a305e2cd9fa29211
matrixfrog <matrixfrog@41e188f2-1ff7-11df-8982-67e453f37615>
2010-04-05 20:50:25 +0000

    implemented goal condition for LightClocksOnTrainLevel and handling of goal condition in main (Relativity) class

--- src/phcs/levels/LightClocksOnTrainLevel.java
+++ src/phcs/levels/LightClocksOnTrainLevel.java
@@ -5,22 +5,32 @@ import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
 
+import phcs.LightClockListener;
+import phcs.Relativity;
 import phcs.RelativityLevel;
 import phcs.gui.VelocitySlider;
 import phcs.objects.LightClock;
 import util.swingutils.RecursiveEnablePanel;
 
-public class LightClocksOnTrainLevel extends RelativityLevel {
+/**
+ * The famous Einstein thought experiment involving two light clocks -- one on a train moving
+ * at a relativistic speed, and one stationary on the ground next to it.
+ */
+// Solution: v = inverseGamma(5.0/4) = 0.6
+public class LightClocksOnTrainLevel extends RelativityLevel implements LightClockListener {
+
+  private boolean goalAchieved = false;
+
   public LightClocksOnTrainLevel() {
+    super();
     setName("Level 1: A light clock on a train.");
 
-    /*
-    private String instructions = "Instructions: The light clock on the left is stationary,\n" +
-        "and it takes 4 seconds for the pulse of light to make a complete cycle.\n" +
-        "You can adjust the speed of the light clock on the right using the slider, which\n" +
-        "shows the clock's speed as a percentage of the speed of light.\n" +
-        "Goal: Set the speed so that the light clock on the right makes a complete cycle in 5 seconds.";
-     */
+    instructions = "Instructions: The light clock on the left is stationary, \n" +
+      "and it takes 4 seconds for the pulse of light to make a complete cycle. \n" +
+      "You can adjust the speed of the light clock on the right using the slider, which \n" +
+      "shows the clock's speed as a percentage of the speed of light. Goal: Adjust the speed \n" +
+      "so that the light clock on the right makes a complete cycle in 5 seconds when viewed \n" +
+      "in the rest frame of the light clock on the left.";
 
     LightClock stationaryLightClock = new LightClock(430, 30);
     stationaryLightClock.setName("LC1");
@@ -28,6 +38,7 @@ public class LightClocksOnTrainLevel extends RelativityLevel {
 
     LightClock movingLightClock = new LightClock(500, 30);
     movingLightClock.setName("LC2");
+    movingLightClock.addLightClockListener(this);
     this.addSimulationObject(movingLightClock);
 
     this.controlPanel = new RecursiveEnablePanel();
@@ -39,8 +50,22 @@ public class LightClocksOnTrainLevel extends RelativityLevel {
   }
 
   @Override
+  public void reset() {
+    super.reset();
+    goalAchieved = false;
+  }
+
+  @Override
   protected boolean goalAchieved() {
-    // TODO LightClocksOnTrainLevel.goalAchieved()
-    return false;
+    return goalAchieved;
+  }
+
+  public void cycleCompleted(int counter) {
+    if (Relativity.DEBUG) {
+      System.out.println("counter: " + counter);
+    }
+    if (499 <= counter && counter < 502) {
+      goalAchieved = true;
+    }
   }
 }
