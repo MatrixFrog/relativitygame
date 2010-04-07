d59ee1b0349043d1fac0f72617cf7b38bad97a05
matrixfrog <matrixfrog@41e188f2-1ff7-11df-8982-67e453f37615>
2010-04-07 05:50:06 +0000

    goal condition for level 2 encoded but untested

---
+++ src/phcs/objects/Grid.java
@@ -0,0 +1,9 @@
+package phcs.objects;
+
+/**
+ * This is not actually a physical object but it's in the 'objects' package because it is
+ * drawn on the screen as though it were an object.
+ */
+public class Grid {
+
+}
