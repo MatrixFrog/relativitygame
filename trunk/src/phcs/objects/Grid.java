22abc5d824f1c925a1a90c5f10b0fe6aa2c96c9d
matrixfrog <matrixfrog@41e188f2-1ff7-11df-8982-67e453f37615>
2010-04-21 19:32:29 +0000

    VerticalGrid complete

--- trunk/src/phcs/objects/Grid.java
+++ trunk/src/phcs/objects/Grid.java
@@ -3,5 +3,8 @@ package phcs.objects;
 import java.awt.Graphics;
 
 public interface Grid {
+
+  double increment = 50;
+
   void paint(Graphics g);
 }
--- trunk/src/phcs/objects/VerticalGrid.java
+++ trunk/src/phcs/objects/VerticalGrid.java
@@ -1,5 +1,48 @@
 package phcs.objects;
 
-public class VerticalGrid {
-  // TODO VerticalGrid
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class VerticalGrid implements Grid {
+
+  private double _x, _y, width, height;
+
+  public VerticalGrid() {
+    this(50, 0, 15, 500);
+  }
+
+  public VerticalGrid(double x, double y, double width, double height) {
+    this._x = x;
+    this._y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  public void paint(Graphics g) {
+    g.setColor(Color.BLACK);
+    g.drawLine(
+        getX() + getWidth()/2, getY(),
+        getX() + getWidth()/2, getY() + getHeight()
+    );
+    for (int y = this.getY(); y < (this.getY() + this.getHeight()); y += increment) {
+      g.drawLine(getX(), y, getX() + getWidth(), y);
+      g.drawString("" + y, getX() + getWidth() + 10, y);
+    }
+  }
+
+  public int getX() {
+    return (int) _x;
+  }
+
+  public int getY() {
+    return (int) _y;
+  }
+
+  public int getWidth() {
+    return (int) width;
+  }
+
+  public int getHeight() {
+    return (int) height;
+  }
 }
