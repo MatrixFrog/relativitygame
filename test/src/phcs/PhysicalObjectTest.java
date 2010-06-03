6308538149f512147a1727c28a40aac622b61de9
matrixfrog <matrixfrog@41e188f2-1ff7-11df-8982-67e453f37615>
2010-06-03 07:02:11 +0000

    upgraded to junit 4.8 and Theories. so fancy

--- test/src/phcs/PhysicalObjectTest.java
+++ test/src/phcs/PhysicalObjectTest.java
@@ -1,21 +1,54 @@
 package phcs;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 import static phcs.PhysicalObject.gamma;
 import static phcs.PhysicalObject.inverseGamma;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+@RunWith(Theories.class)
 public class PhysicalObjectTest {
 
-  /**
-   * Just a test to confirm that inverseGamma() is in fact the inverse of gamma()
-   */
-  @Test
-  public void testGamma() {
-    assertEquals(1, gamma(0), 0);
-    for (double d = 0; d < 1; d += 0.001) {
-      assertEquals(d, inverseGamma(gamma(d)), 0.00000001);
+  @DataPoints
+  public static Double[] data() {
+    List<Double> list = new ArrayList<Double>();
+    for (double d = 0; d < 1; d+= 0.01) {
+      list.add(d);
     }
+    return list.toArray(new Double[list.size()]);
+  }
+
+  /**
+   * Confirming that inverseGamma() is in fact the inverse of gamma()
+   */
+  @Theory
+  public void testGamma(double d) {
+    assertEquals(d, inverseGamma(gamma(d)), 0.00000001);
+  }
+
+  /**
+   * Confirming that gamma() is a monotonically increasing function
+   */
+  @Theory
+  public void testGammaIncreasing(double smaller, double larger) {
+    assumeTrue(smaller < larger);
+    assertTrue(gamma(smaller) < gamma(larger));
+  }
+
+  @Test
+  public void testGammaZeroIsOne() {
+    assertThat(gamma(0), is(1.0));
   }
 
 }
